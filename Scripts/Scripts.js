b56d5da76515e5b8812666be860992106690e965
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2022-10-03 21:16:56 +0200

    Update Scripts.js

--- Scripts/Scripts.js
+++ Scripts/Scripts.js
@@ -1,15 +1,17 @@
-for (let table of document.querySelectorAll('table')) {
-      for (let th of table.tHead.rows[0].cells) {
+// This script of sor sorting the tables by the table heads in alphabethical and/or numerical order
+
+for (let table of document.querySelectorAll('table')) { // selecting the entire table
+      for (let th of table.tHead.rows[0].cells) { // choosing to make something happen when clicking on the table headers
         th.onclick = function(){
-          const tBody = table.tBodies[0];
+          const tBody = table.tBodies[0]; // choosing all the rows and columns
           const rows = tBody.rows;
           for (let tr of rows) {
-            Array.prototype.slice.call(rows)
+            Array.prototype.slice.call(rows) // cutting the table into individual rows
               .sort(function(tr1, tr2){
                 const cellIndex = th.cellIndex;
                 return tr1.cells[cellIndex].textContent.localeCompare(tr2.cells[cellIndex].textContent);
               })
-              .forEach(function(tr){
+              .forEach(function(tr){ // adding the rows in their new position and remiving them from the old one
                 this.appendChild(this.removeChild(tr));
               }, tBody);
           }
